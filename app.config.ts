fdd8604738b98589b7d6d96b0784628b4f776125
cojovi <31863818+cojovi@users.noreply.github.com>
2023-10-31 20:11:24 -0500

    social&link updates

--- app.config.ts
+++ app.config.ts
@@ -1,38 +1,58 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'cojovi',
+    description: 'Come get your daily intake of tech',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
+      twitter: 'the_cojovi',
+      github: 'cojovi',
+
       nuxt: {
         label: 'Nuxt',
         icon: 'simple-icons:nuxtdotjs',
         href: 'https://nuxt.com'
-      }
+      },
+
+      facebook: 'techdonecheap',
+      instagram: 'after5diy',
+      tiktok: 'cojovi',
+      youtube: '@techdonecheap'
     },
+
     github: {
-      dir: '.starters/default/content',
+      dir: '.tree/main/content',
       branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
+      repo: 'cojocrib',
+      owner: 'cojovi',
       edit: true
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
       padded: true,
       fluid: true
     },
+
     header: {
-      logo: true,
+      logo: false,
       showLinkIcon: true,
       exclude: [],
-      fluid: true
+      fluid: true,
+      title: 'cojovi.com'
+    },
+
+    footer: {
+      credits: {
+        icon: 'tabler:binary',
+        text: 'Powered by cojovi',
+        href: 'https://cojovi.com'
+      }
     }
   }
 })
