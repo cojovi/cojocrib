9d8bdc4641e01623c7245ec62a624b9a54736a46
cojovi <31863818+cojovi@users.noreply.github.com>
2024-01-09 16:51:30 -0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,11 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
   devtools: { enabled: true },
-  
+
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible',
-    '@nuxthq/studio',
-    '@nuxtjs/google-analytics'
+    '@nuxthq/studio'
   ]
 })
-
